b483e009ada96225d8d86494b8a60db0558acdb1
András Németh <sunarch@protonmail.com>
2024-05-15 17:55:40 +0200

    Check for invalid arguments

--- src/main.zig
+++ src/main.zig
@@ -43,6 +43,7 @@ pub fn main() !void {
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
 
+    var invalid_arg_found: bool = false;
     var args_index: u8 = 0;
     for(args) |arg| {
         if (args_index == 0) {
@@ -50,52 +51,61 @@ pub fn main() !void {
             continue;
         }
 
+        // Check options: `if .. else if` from here
+
         if (std.mem.eql(u8, arg, "-v") or
             std.mem.eql(u8, arg, "--version")) {
             cli_options.version = true;
         }
-        if (std.mem.eql(u8, arg, "-h") or
-            std.mem.eql(u8, arg, "--help")) {
+        else if (std.mem.eql(u8, arg, "-h") or
+                 std.mem.eql(u8, arg, "--help")) {
             cli_options.help = true;
         }
-        if (std.mem.eql(u8, arg, "-l") or
-        std.mem.eql(u8, arg, "--list")) {
+        else if (std.mem.eql(u8, arg, "-l") or
+                 std.mem.eql(u8, arg, "--list")) {
             cli_options.list = true;
         }
 
-        if (std.mem.eql(u8, arg, "--debug")) {
+        else if (std.mem.eql(u8, arg, "--debug")) {
             gen_options.debug = true;
         }
 
-        if (std.mem.eql(u8, arg, "-w") or
-            std.mem.eql(u8, arg, "--web")) {
+        else if (std.mem.eql(u8, arg, "-w") or
+                 std.mem.eql(u8, arg, "--web")) {
             gen_options.web = true;
         }
-        if (std.mem.eql(u8, arg, "-i") or
-            std.mem.eql(u8, arg, "--indexed")) {
+        else if (std.mem.eql(u8, arg, "-i") or
+                 std.mem.eql(u8, arg, "--indexed")) {
             gen_options.indexed = true;
         }
-        if (std.mem.eql(u8, arg, "-s") or
-            std.mem.eql(u8, arg, "--spaced")) {
+        else if (std.mem.eql(u8, arg, "-s") or
+                 std.mem.eql(u8, arg, "--spaced")) {
             gen_options.spaced = true;
         }
 
-        if (std.mem.eql(u8, arg, "-c6"))  { gen_options.count =  6; }
-        if (std.mem.eql(u8, arg, "-c7"))  { gen_options.count =  7; }
-        if (std.mem.eql(u8, arg, "-c8"))  { gen_options.count =  8; }
-        if (std.mem.eql(u8, arg, "-c9"))  { gen_options.count =  9; }
-        if (std.mem.eql(u8, arg, "-c10")) { gen_options.count = 10; }
-        if (std.mem.eql(u8, arg, "-c11")) { gen_options.count = 11; }
-        if (std.mem.eql(u8, arg, "-c12")) { gen_options.count = 12; }
+        else if (std.mem.eql(u8, arg, "-c6"))  { gen_options.count =  6; }
+        else if (std.mem.eql(u8, arg, "-c7"))  { gen_options.count =  7; }
+        else if (std.mem.eql(u8, arg, "-c8"))  { gen_options.count =  8; }
+        else if (std.mem.eql(u8, arg, "-c9"))  { gen_options.count =  9; }
+        else if (std.mem.eql(u8, arg, "-c10")) { gen_options.count = 10; }
+        else if (std.mem.eql(u8, arg, "-c11")) { gen_options.count = 11; }
+        else if (std.mem.eql(u8, arg, "-c12")) { gen_options.count = 12; }
 
-        if (std.mem.eql(u8, arg, "-wAGR"))    { try wordlist.set_wordlist(.wordlist_agr_en_original); }
-        if (std.mem.eql(u8, arg, "-wAGRalt")) { try wordlist.set_wordlist(.wordlist_agr_en_alt); }
-        if (std.mem.eql(u8, arg, "-wEFF"))    { try wordlist.set_wordlist(.wordlist_eff_large); }
-        if (std.mem.eql(u8, arg, "-wEFFs1"))  { try wordlist.set_wordlist(.wordlist_eff_short_1); }
-        if (std.mem.eql(u8, arg, "-wEFFs2"))  { try wordlist.set_wordlist(.wordlist_eff_short_2_0); }
+        else if (std.mem.eql(u8, arg, "-wAGR"))    { try wordlist.set_wordlist(.wordlist_agr_en_original); }
+        else if (std.mem.eql(u8, arg, "-wAGRalt")) { try wordlist.set_wordlist(.wordlist_agr_en_alt); }
+        else if (std.mem.eql(u8, arg, "-wEFF"))    { try wordlist.set_wordlist(.wordlist_eff_large); }
+        else if (std.mem.eql(u8, arg, "-wEFFs1"))  { try wordlist.set_wordlist(.wordlist_eff_short_1); }
+        else if (std.mem.eql(u8, arg, "-wEFFs2"))  { try wordlist.set_wordlist(.wordlist_eff_short_2_0); }
+
+        else {
+            std.debug.print("Error: Unrecognized CLI argument: '{s}'\n", .{ arg });
+            invalid_arg_found = true;
+        }
 
         args_index += 1;
     }
+
+    if (invalid_arg_found) { return; }
 
     try wordlist.populate(&dict);
 
